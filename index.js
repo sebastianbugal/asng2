28de99c0286411f3a24c10e297812de35cd4278d
Sebastian Bugal <seb1001@SebastiansMBP2.hitronhub.home>
2020-06-18 00:31:39 -0700

    user entry progress

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const express = require('express')
 const path = require('path')
-const PORT = process.env.PORT || 1100
+const PORT = process.env.PORT || 1000
 const {Pool}=require('pg');
 var pool;
+try{
 pool=new Pool({
   connectionString: process.env.DATABASE_URL
-});
+});}
+finally{
+  pool=new Pool({
+    connectionString: 'postgres://postgres:root@localhost/users'
+  });}
+
 
 var app=express()
 app.use(express.json());
@@ -13,21 +19,37 @@ app.use(express.urlencoded({extended:false}));
 app.use(express.static(path.join(__dirname, 'public')))
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
-app.get('/', (req, res) => res.render('pages/index'))
-app.get('/database',(req,res) => {
+app.get('/display',(req,res) => {
   var getUsrQuery =`SELECT * FROM usr`; 
   pool.query(getUsrQuery,(error,result)=>{
     if(error){
       res.end(error);}
     var results={'rows':result.rows}
-    res.render('pages/db',results);
+    res.render('pages/display',results);
+  
+  })
+});
+app.get('/',(req,res) => {
+  var getUsrQuery =`SELECT * FROM usr`; 
+  pool.query(getUsrQuery,(error,result)=>{
+    if(error){
+      res.end(error);}
+    var results={'rows':result.rows}
+    res.render('pages/user_enter',results);
     
   })
 });
+
+
 app.post('/adduser',(req,res)=>{
   var uname=req.body.uname;
   var age=req.body.age;
-  res.send(`username: ${uname}, age: ${age}`);
+  var insert_user_query=`insert into usr (name, age) values('${uname}',${age})`;
+  pool.query(insert_user_query,(error,result)=>{
+    if(error){
+      res.end(error);}
+    req.resume();
+})
 
 });
 app.get('/users/:id',(req,res)=>{
