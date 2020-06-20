c6c213e92a33f6ce2f13b9048692744335f0a03d
sebastianbugal <49225786+sebastianbugal@users.noreply.github.com>
2020-06-20 01:52:36 -0700

    done

--- public/script.js
+++ public/script.js
@@ -1,8 +1,37 @@
+// function setup(){
+//     doc=document.querySelectorAll(".norm");
+//     for(d of doc){
+//         d.addEventListener("click",function(){
+//             this.style.background = 'rgb(101, 117, 112)';
+//             var uid=document.getElementById('user_selected');
+//             uid.textContent=this.id;
+//             uid.value=this.id;
+//             var ud=document.getElementById('user_selected_up');
+//             ud.textContent=this.id;
+//             ud.value=this.id;
+//             var u=document.getElementById('user_selected_info');
+//             u.textContent=this.id;
+//             u.value=this.id;
+//             doc=document.querySelectorAll(".norm");
+//             for(d of doc){
+//                 console.log(d.style.background)
+//                 if((d.style.background=='rgb(101, 117, 112)' || d.style.background=='rgb(101, 117, 112) none repeat scroll 0% 0%') && d.id!=this.id ){
+//                     console.log(d.dataset.col)
+                    
+//                     d.style.background=d.dataset.col;
+//                 }
+//             }
+
+//         })    
+//     }
+    
+// }
+// setup();
 function setup(){
     doc=document.querySelectorAll(".norm");
     for(d of doc){
         d.addEventListener("click",function(){
-            this.style.background = 'rgb(101, 117, 112)';
+            this.style.border = '4px solid orange';
             var uid=document.getElementById('user_selected');
             uid.textContent=this.id;
             uid.value=this.id;
@@ -15,10 +44,10 @@ function setup(){
             doc=document.querySelectorAll(".norm");
             for(d of doc){
                 console.log(d.style.background)
-                if((d.style.background=='rgb(101, 117, 112)' || d.style.background=='rgb(101, 117, 112) none repeat scroll 0% 0%') && d.id!=this.id ){
+                if((d.style.border=='4px solid orange' || d.style.background=='4px solid orange none repeat scroll 0% 0%') && d.id!=this.id ){
                     console.log(d.dataset.col)
                     
-                    d.style.background=d.dataset.col;
+                    d.style.border="4px solid black"
                 }
             }
 
